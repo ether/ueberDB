d1ac066496f92c567528638aa9c448d8cfdab15d
Philip Claren <kobe@mixxt.net>
2012-02-07 12:40:33 +0100

    Added debug output, package.json requirements, default settings

--- redis_db.js
+++ redis_db.js
@@ -35,18 +35,20 @@
 var redis = require("redis");
 
 exports.database = function(settings) {
-  this.client = null;
+    this.client = null;
 
-  if(!settings) settings = {};
-  if(!settings.database) settings.database = 0;
-  if(!settings.host) settings.host = '127.0.0.1';
-  if(!settings.port) settings.port = 6379;
+    if(!settings) settings = {};
+    if(!settings.database) settings.database = 0;
+    if(!settings.host) settings.host = '127.0.0.1';
+    if(!settings.port) settings.port = 6379;
 
-  this.settings = settings;
+    this.settings = settings;
 }
 
 exports.database.prototype.init = function(callback) {
+    console.debug("Trying to connect to redis on " + this.settings.host + ":" + this.settings.port);
     this.client = redis.createClient(this.settings.port, this.settings.host);
+
     this.client.database = this.settings.database;
 
     this.client.on("error", function(err) {
@@ -54,24 +56,44 @@ exports.database.prototype.init = function(callback) {
     });
 
     this.client.on("connect", function() {
-        console.log(this.database);
+        console.debug("Connected to redis, now switching to database " + this.database);
         this.select(this.database);
         callback();
     });
 }
 
 exports.database.prototype.get = function (key, callback) {
+    console.debug("redis.get for key '" + key + "'");
     this.client.get(key, function(err, val) {
-        if(!err) callback(null, val);
+        if(err) {
+            console.error(err);
+        } else {
+            console.debug("redis.get result '" + val + "'");
+        }
+        callback(err?err:null, val);
     });
 }
 
 exports.database.prototype.set = function (key, value, callback) {
-    this.client.set(key,value,callback);
+    this.client.set(key,value,function(err){
+        if(err) {
+            console.error(err);
+        } else {
+            console.debug("redis.set '" + key + "' => '" + val + "'");
+        }
+        callback(err?err:null);
+    });
 }
 
 exports.database.prototype.remove = function (key, callback) {
-    this.client.del(key,callback);
+    this.client.del(key,function(err) {
+        if(err) {
+            console.error(err);
+        } else {
+            console.debug("redis.del '" + key + "' => '" + val + "'");
+        }
+        callback(err?err:null);
+    });
 }
 
 exports.database.prototype.doBulk = function (bulk, callback) {
@@ -85,9 +107,23 @@ exports.database.prototype.doBulk = function (bulk, callback) {
         }
     }
 
-    multi.exec(callback);
+    multi.exec(function(err) {
+        if(err) {
+            console.error(err);
+        } else {
+            console.debug("redis.multi",bulk);
+        }
+        callback(err?err:null);
+    });
 }
 
 exports.database.prototype.close = function(callback) {
-    this.client.end(callback);
+    this.client.end(function(err){
+        if(err) {
+            console.error(err);
+        } else {
+            console.debug('Closed connection to redis server.');
+        }
+        callback(err?err:null);
+    });
 }
