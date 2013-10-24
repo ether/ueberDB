d7c59e037e69e7c19dfa5d929386ff7a4ad1477d
Kenan Sulayman <kenan@sly.mn>
2013-10-24 18:34:57 +0200

    Optimizes lmdb-initialization and removes async dependency

--- lmdb_db.js
+++ lmdb_db.js
@@ -1,5 +1,5 @@
 /**
- * 2011 Peter 'Pita' Martischka 
+ * 2011 Peter 'Pita' Martischka
  * 2013 Kenan Sulayman
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,10 +17,10 @@
 /**
  * LevelDB port of UeberDB
  * See http://code.google.com/p/leveldb/ for information about LevelDB
- * 
+ *
  * LevelDB must be installed in order to use this database.
  * Install it using npm install uberlevel or npm install level-lmdb
- * 
+ *
  * Options:
  *   directory: The LevelDB directory, defaults to "lmdb.db"
  *   create_if_missing: Create the LevelDB directory (but not parent directories)
@@ -32,64 +32,62 @@
 
 try {
   var lmdb = require("uberlevel") || require("level-lmdb");
-} catch(e) {
+} catch (e) {
   console.error("FATAL: The lmdb dependency could not be found. Please install using npm install uberlevel.");
   process.exit(1);
 }
 
-var async = require("async");
-
 exports.database = function(settings) {
-  this.db=null;
-  
-  if(!settings || !settings.directory) {
-    settings = {directory:"leveldb-store",create_if_missing: true};
+  this.db = null;
+
+  if (!settings || !settings.directory) {
+    settings = {
+      directory: "lmdb-store",
+      create_if_missing: true
+    };
   }
-  
+
   this.settings = settings;
 }
 
 exports.database.prototype.init = function(callback) {
   var _this = this;
-  async.waterfall([
-    function(callback) {
-      leveldb.open(_this.settings.directory, { create_if_missing: true },
-         function(err, db) {
-           _this.db = db;
-           callback(err);
-	 });
+
+    lmdb(_this.settings.directory, {
+      createIfMissing: _this.settings.create_if_missing
+    }, function(err, db) {
+      _this.db = db;
+      callback(err);
+    })
+  }
+
+  exports.database.prototype.get = function(key, callback) {
+    this.db.get(key, function(err, value) {
+      callback(err, value ? value : null);
+    });
+  }
+
+  exports.database.prototype.set = function(key, value, callback) {
+    this.db.put(key, value, callback);
+  }
+
+  exports.database.prototype.remove = function(key, callback) {
+    this.db.del(key, callback);
+  }
+
+  exports.database.prototype.doBulk = function(bulk, callback) {
+    var batch = this.db.batch();
+    bulk.forEach(function(entity) {
+      if (entity.type == "set") {
+        batch.put(entity.key, entity.value);
+      } else if (entity.type == "remove") {
+        batch.del(entity.key);
       }
-  ],callback);
-}
+    })
+    this.db.write(batch, callback);
+  }
 
-exports.database.prototype.get = function (key, callback) {
-  this.db.get(key, function(err, value) {
-    callback(err, value ? value : null);
-  });
-}
-
-exports.database.prototype.set = function (key, value, callback) {
-  this.db.put(key, value, callback);
-}
-
-exports.database.prototype.remove = function (key, callback) {
-  this.db.del(key, callback);
-}
-
-exports.database.prototype.doBulk = function (bulk, callback) {
-  var batch = this.db.batch();
-  bulk.forEach(function (entity) {
-    if(entity.type == "set") {
-      batch.put(entity.key, entity.value);
-    }
-    else if(entity.type == "remove") {
-      batch.del(entity.key);
-    }
-  })
-  this.db.write(batch, callback);
-}
-
-exports.database.prototype.close = function(callback) {
-  delete this.db;
-  callback(null)
-}
+  exports.database.prototype.close = function(callback) {
+    delete this.db;
+    callback(null)
+  }
