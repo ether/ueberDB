e8b58d03534ade8d83c2d1946a8350a23952531e
John McLear <john@mclear.co.uk>
2020-04-01 18:05:33 +0000

    go go gadget mysql fix for 2877

--- mysql_db.js
+++ mysql_db.js
@@ -129,7 +129,7 @@ exports.database.prototype.init = function(callback)
 
 exports.database.prototype.get = function (key, callback)
 {
-  this.db.query("SELECT `value` FROM `store` WHERE  `key` = ?", [key], function(err,results)
+  this.db.query("SELECT `value` FROM `store` WHERE BINARY `key` = ?", [key], function(err,results)
   {
     var value = null;
     
@@ -146,7 +146,7 @@ exports.database.prototype.get = function (key, callback)
 
 exports.database.prototype.findKeys = function (key, notKey, callback)
 {
-  var query="SELECT `key` FROM `store` WHERE  `key` LIKE ?"
+  var query="SELECT `key` FROM `store` WHERE BINARY `key` LIKE ?"
     , params=[]
   ;
   
