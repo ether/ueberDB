29ee8fd79c2900552bea4a5c0d9df6680aca5569
John McLear <john@mclear.co.uk>
2020-05-01 21:15:54 +0000

    first basic test

--- test/lib/tests.js
+++ test/lib/tests.js
@@ -1,26 +1,29 @@
+const Randexp = require("randexp");
+
 exports.tests = {
-  someTest: function(db){
+  someTest: function(db, assert, test){
+    let randomVal = new Randexp(/.+/).gen();
+    // console.warn(randomVal);
   },
-  someOtherTest: function(db){
+  basicReadWrite: function(db, assert, test){
+  var input = {a:1,b: new Randexp(/.+/).gen()};
+  var key =  new Randexp(/.+/).gen();
+  db.set( key , input );
+
+  //get the object
+  db.get(key, function(err, output){
+    describe(test, () => {
+      it('Does a basic write->read operation with a random key/value', () => {
+        let matches = JSON.stringify(input) === JSON.stringify(output);
+        assert.equal(matches, true);
+      });
+    });
+  });
 
   }
 }
 
 exports.runTest = async function(test, db){
-  var input = {a:1,b:2};
-  await db.set("valueA" , input );
-
-  //get the object
-  db.get("valueA", function(err, output){
-
-    describe(test, () => {
-console.log("foo", input, output);
-      it('should pass obv..', () => {
-        expect(JSON.stringify.input).to.equal(JSON.stringify(output));
-      });
-    });
-
-  });
 
   db.close(function(){
     process.exit(0);
--- test/testrunner.js
+++ test/testrunner.js
@@ -1,17 +1,8 @@
+const assert    = require('assert');
 const expect    = require('chai').expect
 const databases = require('./lib/databases.js').databases;
 const tests     = require('./lib/tests.js');
-const Randexp   = require('randexp');
 const ueberDB   = require('../CloneAndAtomicLayer.js');
-
-let randomVal = new Randexp(/.+/).gen();
-console.warn(randomVal);
-
-  describe('test should fail', () => {
-    it('should return a string', () => {
-      expect('ci with travis').to.equal('ci with travisa');
-    });
-  });
 
 // For each Database (gets database settings)
 for (const database in databases){
@@ -19,18 +10,17 @@ for (const database in databases){
   // connect to database
   var db = new ueberDB.database(database, dbSettings[database])
 
-  db.init(async function (err){
+  db.init(function (err){
     if(err){
       console.error(err);
       process.exit(1);
     }
     for (const test in tests.tests){
-      console.warn("test", test);
-      tests.runTest(test, db);
+      var testFn = tests.tests[test];
+      testFn(db, assert, test);
     }
   });
 }
-
 /*
 Test approaches:
   * Fuzzed inc. whitespace
@@ -44,16 +34,3 @@ Methods:
   > set and remove
 */
 
-  // For each test [tests.js]
-    // Run the test
-  // Disconnect from database
-
-/*
-function tests(db){
-  describe('test', () => {
-    it('should return a string', () => {
-      expect('ci with travis').to.equal('ci with travis');
-    });
-  });
-}
-*/
