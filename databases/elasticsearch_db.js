8dd0caf612975d430375679af53dd4e8fdddc1c6
John McLear <john@mclear.co.uk>
2020-05-05 10:01:15 +0000

    lkdsjf git commit -m lkdsjf22

--- databases/elasticsearch_db.js
+++ databases/elasticsearch_db.js
@@ -24,7 +24,7 @@ var elasticsearchSettings = {
   
   // for a list of valid API values see:
   // https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html#config-options
-  api           : '2.4'
+  api           : '7.6'
 };
 
 var client;
