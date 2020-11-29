5094893cd5714567ba868cfca534d6521c573715
John McLear <john@mclear.co.uk>
2020-11-29 21:49:45 +0000

    Fix use of undefined variable

--- databases/postgres_common.js
+++ databases/postgres_common.js
@@ -136,6 +136,7 @@ exports.findKeys = function(key, notKey, callback)
 
 exports.set = function(key, value, callback)
 {
+  const _this = this;
   if(key.length > 100)
   {
     callback("Your Key can only be 100 chars");
