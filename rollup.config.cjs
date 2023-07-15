7e6cdb137cab22ea4f8263f4fd98a7c084adaef5
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-07-15 23:47:50 +0200

    Fixed.

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -2,6 +2,7 @@
 // eslint-disable-next-line strict
 const typescript = require('rollup-plugin-typescript2');
 const glob = require('glob');
+const nodeResolve = require('@rollup/plugin-node-resolve').default;
 
 module.exports = {
   input: ['./index.ts'].concat(glob.sync('./databases/*.ts')).concat(glob.sync('./test/*.ts')), // Matches all TypeScript files in the 'src' directory and its subdirectories
