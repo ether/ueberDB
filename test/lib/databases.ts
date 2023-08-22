9df086047bf0b2753c84ceddf86aef6b5bceadd8
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-08-22 14:39:38 +0200

    Updated speed constraints.

--- test/lib/databases.ts
+++ test/lib/databases.ts
@@ -51,9 +51,9 @@ export const databases:DatabaseType = {
     url: 'mongodb://127.0.0.1:27017',
     database: 'mydb_test',
     speeds: {
-      setMax: 0.2,
-      getMax: 0.05,
-      removeMax: 0.3,
+      setMax: 0.5,
+      getMax: 0.1,
+      removeMax: 0.6,
     },
   },
   couch: {
